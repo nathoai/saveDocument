95c980a086f50f331d8c1db72c7bb31efde76537
nathoai <56080363+nathoai@users.noreply.github.com>
2019-10-02 21:24:02 +0700

    Add files via upload

---
+++ Project1.Utilities/Valid.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Project1.Utilities
+{
+    public static class Valid
+    {
+        public static string ToAlphaNumericOnly(this string input)
+        {
+            Regex rgx = new Regex("[^a-zA-Z0-9]");
+            return rgx.Replace(input, "");
+        }
+
+        public static string ToAlphaOnly(this string input)
+        {
+            Regex rgx = new Regex("[^a-zA-Z]");
+            return rgx.Replace(input, "");
+        }
+
+        public static string ToNumericOnly(this string input)
+        {
+            Regex rgx = new Regex("[^0-9]");
+            return rgx.Replace(input, "");
+        }
+        
+
+    }
+}
